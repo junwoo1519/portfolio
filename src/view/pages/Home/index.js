a6893d8d00c287065d005b0a1c3f197fc0668a23
junwoo1519 <junwoo101797@gmail.com>
2021-02-18 22:44:24 +0900

    about section 추가

--- src/view/pages/Home/index.js
+++ src/view/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components';
 import Visual from "../../components/Sections/Visual";
+import About from "../../components/Sections/About";
 
 const Home = () => {
     
     return (
         <Container>
             <Visual/>
+            <About/>
         </Container>
     )
 }
